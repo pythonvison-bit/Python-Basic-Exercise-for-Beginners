f67aff74dc48c0f317e97e1f1b71cbffa7d6ce49
Abhijeet <python.vison@gmail.com>
2026-09-25 11:06:07 +0530

    Add files via upload

---
+++ CHAPTER 10.py
@@ -0,0 +1,21 @@
+class Employ:  # This is a class attributes.
+    company = "microsoft"
+    salary = "90 lpa"
+
+abhijeet = Employ() # This is an object atributes.
+abhijeet.name = 'abhijeet'
+print(abhijeet.salary, abhijeet.company)
+
+# __init__() constructor
+
+class Employee: 
+def __init__(self, name): 
+self.name=name 
+def getSalary(self): 
+... 
+harry = Employee("Harry")
+
+
+
+
+
